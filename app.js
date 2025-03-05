b2191a1626e12b0f3841e1c08066a9dd3d0d87bc
web3kitty <188905121+web3kitty@users.noreply.github.com>
2025-03-05 18:12:08 +0100

    uploading core website files

---
+++ app.js
@@ -0,0 +1,7 @@
+
+const menuToggle = document.getElementById('menu-toggle');
+const mobileMenu = document.getElementById('mobile-menu');
+
+menuToggle.addEventListener('click', () => {
+    mobileMenu.classList.toggle('hidden');
+});

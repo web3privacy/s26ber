cde4695ce5977eab10e500ffa505943a77a5dee2
web3kitty <188905121+web3kitty@users.noreply.github.com>
2025-03-06 12:56:10 +0100

    say no the JS kids

--- app.js
+++
@@ -1,7 +0,0 @@
-
-const menuToggle = document.getElementById('menu-toggle');
-const mobileMenu = document.getElementById('mobile-menu');
-
-menuToggle.addEventListener('click', () => {
-    mobileMenu.classList.toggle('hidden');
-});
